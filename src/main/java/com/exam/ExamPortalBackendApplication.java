07a032bdd6b4da567ecb34834c74dd4424aa78a4
DELL <ahmedraza.razdar@gmail.com>
2025-11-20 15:23:41 +0100

    commit demo

--- src/main/java/com/exam/ExamPortalBackendApplication.java
+++ src/main/java/com/exam/ExamPortalBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamPortalBackendApplication {
 
-    //main class
+    //main class file
 	public static void main(String[] args) {
 		SpringApplication.run(ExamPortalBackendApplication.class, args);
 
