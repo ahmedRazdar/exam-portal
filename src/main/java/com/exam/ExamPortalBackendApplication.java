58b6c91b83af6b6e13023d9e0f3905c8dac4e2b3
DELL <ahmedraza.razdar@gmail.com>
2025-11-20 15:22:58 +0100

    welcome file added

--- src/main/java/com/exam/ExamPortalBackendApplication.java
+++ src/main/java/com/exam/ExamPortalBackendApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamPortalBackendApplication {
 
+    //main class
 	public static void main(String[] args) {
 		SpringApplication.run(ExamPortalBackendApplication.class, args);
-	}
 
+        System.out.println("Application has been started");
+	}
 }
